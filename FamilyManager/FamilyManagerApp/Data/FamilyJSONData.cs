e30231545622ba898c7f7a915696fcdb34839b7e
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2021-10-05 09:44:09 +0200

    Push

---
+++ FamilyManager/FamilyManagerApp/Data/FamilyJSONData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace FamilyManagerApp.Data
+{
+    public class FamilyJSONData : IFamilyData
+    {
+        
+        
+        public IList<Family> GetFamilies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Person> GetPeople()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddFamily(Family family)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddPerson(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFamily(string streetName, int houseNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePerson(int personId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Family GetFamily(string streetName, int houseNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Person GetPerson(int personId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateFamily(Family family)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FamilyManager/FamilyManagerApp/Data/IFamilyData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Models;
+
+namespace FamilyManagerApp.Data
+{
+    public interface IFamilyData
+    {
+        IList<Family> GetFamilies();
+        IList<Person> GetPeople();
+        void AddFamily(Family family);
+        void AddPerson(Person person);
+        void RemoveFamily(string streetName, int houseNumber);
+        void RemovePerson(int personId);
+        Family GetFamily(string streetName, int houseNumber);
+        Person GetPerson(int personId);
+        void UpdateFamily(Family family);
+        void UpdatePerson(Person person);
+    }
+}
